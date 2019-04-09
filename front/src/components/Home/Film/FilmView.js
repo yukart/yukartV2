6489af5d94b6c4686e58035395d4acb3b2e06881
Louis FONTAINE <lf94777@gmail.com>
2019-04-09 11:26:53 +0200

    -- modif

--- front/src/components/Home/Film/FilmView.js
+++ front/src/components/Home/Film/FilmView.js
@@ -10,98 +10,101 @@ import searchFilmByTitle from '../../../actions/searchFilmByTitle.js';
 import Banner from '../Banner';
 
 const style = {
-		paper: {
-		  height: '75%',
-		  width: '75%',
-		  margin: 20,
-		  textAlign: 'center',
-		  display: 'inline-block',
-		},
-		puce: {
-			listStyleType: 'none'
-		}
+	paper: {
+		width: '900px',
+		margin: 20,
+		textAlign: 'center',
+		display: 'block',
+	},
+	puce: {
+		listStyleType: 'none'
+	},
+	image: {
+		width: '300px',
+		height: '445px'
+	}
 };
 
 class FilmView extends React.Component {
 	constructor(props) {
-	    super(props);
-	    this.state = {
-    		open: false,
-    		value: "",
-	    };
-	  }
-  componentWillMount() {
-   
+		super(props);
+		this.state = {
+			open: false,
+			value: "",
+		};
+	}
+	componentWillMount() {
 
-  }
 
-  componentWillUnmount() {
+	}
 
-  }
+	componentWillUnmount() {
 
-  handleChange = (event) => {
-	    this.setState({
-	      value: event.target.value,
-	    });
-	  };
-  handleButton = () => {
-	  this.props.updateListFilms(this.state.value);
-	  
-	  this.setState({
-	  	  value: ""
-	   });
-  };
+	}
 
-  
-  render() {
-    return (
-    			<Paper zDepth={5}>
-    				<Banner appName={this.props.appName}/>
-						<div className="container page">
-	    				<div>
-    					<TextField
-	    		          id="text-field-controlled"
-	    		          value={this.state.value}
-	    		          onChange={this.handleChange}
-	    		        />
-	    			    <RaisedButton label="Search" primary={true} onClick={this.handleButton}/>
-	    			    </div>
-	    				<div className="row">
-								{this.props.film !== null && this.props.film.length !== 0 &&
-									<h1> Your research : </h1>
-								}
+	handleChange = (event) => {
+		this.setState({
+			value: event.target.value,
+		});
+	};
+	handleButton = () => {
+		this.props.updateListFilms(this.state.value);
 
-								{this.props.film !== null && this.props.film.length !== 0 &&
-									this.props.film.map((film) => 
-										<Paper style={style.paper} zDepth={2}>
-											<img alt="poster" width="25%" src={film.poster_url}/>
-											<ul>
-												<li style={style.puce}>Title : {film.title} </li>
-												<li style={style.puce}>Year : {film.year} </li>
-												<li style={style.puce}>Release date : {film.release_date} </li>
-												<li style={style.puce}>Runtime : {film.runtime} </li>
-												<li style={style.puce}>Genre : {film.genre} </li>
-												<li style={style.puce}>Synopsis : {film.synopsis} </li>
-											</ul>
-										</Paper>
-									)
-								}
-								{this.props.film !== null && this.props.film === "" &&
-									<Paper style={style} zDepth={2}>
-										<p> Movie not found : error 404 !!! </p>
-									</Paper>
-								}
-							</div>
-						</div>
+		this.setState({
+			value: ""
+		});
+	};
+
+
+	render() {
+		return (
+			<Paper zDepth={5}>
+				<Banner appName={this.props.appName} />
+				<div className="container page">
+					<div>
+						<TextField
+							id="text-field-controlled"
+							value={this.state.value}
+							onChange={this.handleChange}
+						/>
+						<RaisedButton label="Search" primary={true} onClick={this.handleButton} />
+					</div>
+
+					{this.props.film !== null && this.props.film.length !== 0 &&
+						<h1> Your research : </h1>
+					}
+					<Paper style={style.paper}>
+
+						{this.props.film !== null && this.props.film.length !== 0 &&
+							this.props.film.map((film) =>
+								<img alt="poster" style={style.image} src={film.poster_url} />
+							)
+						}
 					</Paper>
+					{/*	<ul>
+										<li style={style.puce}>Title : {film.title} </li>
+										<li style={style.puce}>Year : {film.year} </li>
+										<li style={style.puce}>Release date : {film.release_date} </li>
+										<li style={style.puce}>Runtime : {film.runtime} </li>
+										<li style={style.puce}>Genre : {film.genre} </li>
+										<li style={style.puce}>Synopsis : {film.synopsis} </li>
+									</ul> 
+								*/}
+					{this.props.film !== null && this.props.film === "" &&
+						<Paper style={style} zDepth={2}>
+							<p> Movie not found : error 404 !!! </p>
+						</Paper>
+					}
+				</div>
+			</Paper>
 
-    	   
-    );
-  }
+
+		);
+	}
 }
 const mapStateToProps = state => ({
-  appName: state.common.appName,
-  film: state.common.film
+	appName: state.common.appName,
+	film: state.common.film
 });
 
 const mapDispatchToProps = dispatch => ({
