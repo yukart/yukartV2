316b5c39cc397c27ea9d2c50e9ac4ce81e64c87a
raniabe31 <htballa@gmail.com>
2019-04-09 10:43:55 +0200

    spotifyAccessAPI

---
+++ src/main/java/fr/dauphine/sia2/yukArt/objects/ArtistOBJ.java
@@ -0,0 +1,8 @@
+package fr.dauphine.sia2.yukArt.objects;
+
+public class ArtistOBJ {
+
+	public String name;
+	public String country;
+
+}
---
+++ src/main/java/fr/dauphine/sia2/yukArt/objects/Musique.java
@@ -0,0 +1,8 @@
+package fr.dauphine.sia2.yukArt.objects;
+
+public class Musique {
+
+	public String title;
+	public String country;
+
+}
