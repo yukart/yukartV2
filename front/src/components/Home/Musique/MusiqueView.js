87c5c9bf169d2f58da1743e0c20802ff62eb4e69
Louis FONTAINE <lf94777@gmail.com>
2019-04-15 10:57:26 +0200

    -- Musique View working

--- front/src/components/Home/Musique/MusiqueView.js
+++ front/src/components/Home/Musique/MusiqueView.js
@@ -1,26 +1,16 @@
-import Banner from '../Banner';
-import MainView from '../MainView';
 import React from 'react';
 import { connect } from 'react-redux';
+
+import Paper from 'material-ui/Paper';
+
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
-import SearchAppBar from '../SearchAppBar';
-import Paper from 'material-ui/Paper';
+
+import searchArtist from '../../../actions/searchArtist.js';
+import Banner from '../Banner';
 
 const style = {
-<<<<<<< HEAD
-    paper: {
-      height: '75%',
-      width: '75%',
-      margin: 20,
-      textAlign: 'center',
-      display: 'inline-block',
-    },
-    puce: {
-      listStyleType: 'none'
-    }
-=======
   paper: {
     height: '75%',
     width: '75%',
@@ -30,15 +20,22 @@ const style = {
   },
   puce: {
     listStyleType: 'none'
+  },
+  gridContainer: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)'
+  },
+  gridItem: {
+    textAlign: 'center',
   }
->>>>>>> branch 'master' of https://github.com/yukart/yukartV2.git
 };
 
 class MusiqueView extends React.Component {
 	constructor(props) {
 	    super(props);
 	    this.state = {
-    		open: false,
+        open: false,
+        value: "",
 	    };
     }
     
@@ -52,23 +49,44 @@ class MusiqueView extends React.Component {
 
   }
 
-  handleChangeDrawer = () => {
-	  this.setState({open: !this.state.open});
-  }
-  
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+    
+  handleButton = () => {
+    this.props.updateListArtist(this.state.value);
+    
+    this.setState({
+        value: ""
+    });
+  };
   render() {
 	  return (
   			<Paper zDepth={5}>
             <Banner appName={this.props.appName}/>
             <div className="container page">
-              <div>
-              <TextField
-                    id="text-field-controlled"
-                    value={this.state.value}
-                    onChange={this.handleChange}
-                  />
-                <RaisedButton label="Search" primary={true} onClick={this.handleButton}/>
+              <div style={style.gridContainer}>
+                <div style={style.gridItem}>
+                  <TextField
+                      id="text-field-controlled"
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                    />
+                  <RaisedButton label="Search" primary={true} onClick={this.handleButton}/>
                 </div>
+              
+                <b style={style.gridItem}>OU</b>
+                <div style={style.gridItem}>
+                  <TextField
+                        id="text-field-controlled"
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                      />
+                  <RaisedButton label="Search" primary={true} onClick={this.handleButton}/>
+                </div>
+              </div>
               <div className="row">
                 {this.props.artist !== null && this.props.artist.length !== 0 &&
                   <h1> Your research : </h1>
@@ -77,10 +95,14 @@ class MusiqueView extends React.Component {
                 {this.props.artist !== null && this.props.artist.length !== 0 &&
                   this.props.artist.map((artist) => 
                     <Paper style={style.paper} zDepth={2}>
-                      <img alt="poster" width="25%" src={artist.images}/>
+                      {artist.images[0] !== undefined &&
+                        <img alt="poster" width="25%" src={artist.images[0].url}/>
+                      }
                       <ul>
                         <li style={style.puce}>Name : {artist.name} </li>
-                        <li style={style.puce}>Genres : {artist.genres} </li>
+                        <li style={style.puce}>Genres : {artist.genres.map((genre) => genre+"; ")} </li>
+                        <li style={style.puce}>Popularité: {artist.popularity}%</li>
+                        <li style={style.puce}> <a href={artist.externalUrls.externalUrls.spotify} target="_blank" alt="lien spotify"> Ecouter cet artiste</a>  </li>
                       </ul>
                     </Paper>
                   )
@@ -100,10 +122,11 @@ class MusiqueView extends React.Component {
 }
 const mapStateToProps = state => ({
   appName: state.common.appName,
+  artist: state.common.artist,
 });
 
 const mapDispatchToProps = dispatch => ({
- 
+  updateListArtist: (name) => dispatch(searchArtist(name)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(MusiqueView);
