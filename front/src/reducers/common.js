1347792ff73003ddf87bac2c1516c104fa51b59f
raniabe31 <htballa@gmail.com>
2019-04-15 09:37:13 +0200

    musique

--- front/src/reducers/common.js
+++ front/src/reducers/common.js
@@ -1,21 +1,22 @@
 
 const defaultState = {
   appName: 'YukArt',
-  film: []
+  film: [],
+  artist: []
 };
 
 export default (state = defaultState, action) => {
   switch (action.type) {
-    case "ADD_FILM":
-    	state.films.push(action.film);
-    	return {
-	    	...state
-    	};
     case "FETCH_FILM_SUCCESS":
     	state.film = action.items;
     	return {
 	    	...state
     	};
+      case "FETCH_ARTIST_SUCCESS":
+      state.artist = action.items;
+      return {
+        ...state
+      };
     default:
       return state;
   }
