8297bd6eed32e4d36a054ce5a8b1ec4575cf981e
Kantoki <victorcdauphine@gmail.com>
2019-04-17 08:57:10 +0200

    Add database connection

---
+++ src/main/java/fr/dauphine/sia2/yukArt/data/DataBase.java
@@ -0,0 +1,45 @@
+package fr.dauphine.sia2.yukArt.data;
+
+import fr.dauphine.sia2.yukArt.engine.Comment;
+import fr.dauphine.sia2.yukArt.engine.User;
+
+public class DataBase {
+
+	private static DataBase INSTANCE = null;
+	private DBConnect dataBConnect;
+
+	private DataBase() {
+		this.dataBConnect = new DBConnect();
+	}
+
+	public static DataBase getDataBase() {
+		if (INSTANCE == null)
+			INSTANCE = new DataBase();
+		return INSTANCE;
+	}
+
+	public boolean existLogin(String login) {
+		return this.dataBConnect.existLogin(login);
+	}
+
+	public boolean existUser(String login, String pass) {
+		return this.dataBConnect.existUser(login, pass);
+	}
+
+	public void createNewUser(String formPseudo, String formPwd, String formMail) {
+		this.dataBConnect.createUser(formPseudo, formPwd, formMail);
+	}
+
+	public User getUser(String formPseudo) {
+		return this.dataBConnect.getUser(formPseudo);
+	}
+
+	public void changePassword(String login, String password) {
+		dataBConnect.changePassword(login, password);
+	}
+
+	public void insertComment(Comment c) {
+		dataBConnect.insertComment(c);
+	}
+
+}
